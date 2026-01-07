04b867708ca89f672df49f01c93ae8eb81af913b
PatelSnehV <snehpatel1233@gmail.com>
2026-01-07 13:17:04 +0530

    Frontend URL added

--- Server/app.js
+++ Server/app.js
@@ -9,7 +9,7 @@ const app = express();
 const Port = 3000;
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "https://watch-wise-c6kn.vercel.app/"
 }));
 app.use("/", preferenceRoutes)
 
